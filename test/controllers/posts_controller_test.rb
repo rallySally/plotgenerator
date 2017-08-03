dc6018b2ef6a7fa7ffe0790e02dbdcb2f138fcfa
stenod <brummelbummel@hotmail.de>
2017-08-03 17:55:32 +0200

    first tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @post = posts(:one)
+    @post = posts(:post1)
   end
 
   test "should get index" do
---
+++ test/features/happy_path_feature_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class OneClickLogTest < ActionDispatch::IntegrationTest
+  include ActiveJob::TestHelper
+
+  test "log habit without any logs yet" do
+    post = posts(:post1)
+
+
+  end
+end
