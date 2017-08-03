4fbd1ae8e3585719f76fd11929b715ca742e895b
Tobias Steuer <t.steuer88@googlemail.com>
2017-08-03 22:12:37 +0200

    HTML Bugfix, first attempt to get some data from db and show it in view

--- app/controllers/persongenerator_controller.rb
+++ app/controllers/persongenerator_controller.rb
@@ -3,5 +3,15 @@ class PersongeneratorController < ApplicationController
   end
 
   def index
+
+    @person = Person.find(1);
+
+
+    @name = Name.where(isSurname: false).first.name + ' ' + Name.where(isSurname: true).first.name
+    @age = @person.age
+    @gender = "male"
+    @job = "Tenns"
+    @skin = @person.color
+    @hair = @person.hair
   end
 end
