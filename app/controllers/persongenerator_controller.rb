94548d783c67aa54a8e451843ba6d7b9a4574439
stenod <brummelbummel@hotmail.de>
2017-08-04 09:22:22 +0200

    ...

--- app/controllers/persongenerator_controller.rb
+++ app/controllers/persongenerator_controller.rb
@@ -1,22 +1,20 @@
 class PersongeneratorController < ApplicationController
-  def generator
-  end
+  def generator; end
 
   def index
-
     @names = Name.where(isSurname: false)
     @surnames = Name.where(isSurname: true)
-    $pickedName = @names[rand(@names.count())]
-    @nameCount = Name.where(isSurname: false).count();
-    @genders = ["","männlich","weiblich"]
-    @colors = ["blass","hell","gebräunt","dunkel","kotz grün","schweinchen rosa", "Kellerbräune"]
-    @hairstyles = ["Lange Mähne", "Kurzer Igel","Undercut","Affro","Vokuhila","schulterlang","Pferdeschwanz","Glatze","Haarkranz","Wilde Sturmfrisur"]
+    $pickedName = @names[rand(@names.count)]
+    @nameCount = Name.where(isSurname: false).count
+    @genders = ['', 'männlich', 'weiblich']
+    @colors = ['blass', 'hell', 'gebräunt', 'dunkel', 'kotz grün', 'schweinchen rosa', 'Kellerbräune']
+    @hairstyles = ['Lange Mähne', 'Kurzer Igel', 'Undercut', 'Affro', 'Vokuhila', 'schulterlang', 'Pferdeschwanz', 'Glatze', 'Haarkranz', 'Wilde Sturmfrisur']
 
-    @name = $pickedName.name + ' ' + @surnames[rand(@surnames.count())].name
+    @name = $pickedName.name + ' ' + @surnames[rand(@surnames.count)].name
     @age = rand(122)
     @gender = @genders[$pickedName.gender]
-    @job = Job.find(rand(Job.count())).name
-    @skin = @colors[rand(@colors.count())]
-    @hair = @hairstyles[rand( @hairstyles.count())]
+    @job = Job.find(rand(1..Job.count)).name
+    @skin = @colors[rand(@colors.count)]
+    @hair = @hairstyles[rand(@hairstyles.count)]
   end
 end
